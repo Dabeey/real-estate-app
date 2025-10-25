deb3a881a94d591371c5e734665778f1af738924
Dabeey <abbasdaughter18@gmail.com>
2025-10-25 23:10:04 +0100

    Refactor PropertyForm to enhance structure and usability by introducing sections for Basic Information, Pricing, Location, and Details. Updated field configurations and added new components for improved user experience.

--- app/Filament/Resources/Properties/Schemas/PropertyForm.php
+++ app/Filament/Resources/Properties/Schemas/PropertyForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Properties\Schemas;
 
+use App\Models\Property;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
@@ -9,6 +10,9 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 
@@ -18,80 +22,161 @@ class PropertyForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->required(),
-                Textarea::make('description')
-                    ->required()
-                    ->columnSpanFull(),
-                Select::make('type')
-                    ->options([
-                    'apartment' => 'Apartment',
-                    'house' => 'House',
-                    'condo' => 'Condo',
-                    'land' => 'Land',
-                    'townhouse' => 'Townhouse',
-                    'villa' => 'Villa',
-                    'commercial' => 'Commercial',
-                ])
-                    ->required(),
-                Select::make('listing_type')
-                    ->options(['sale' => 'Sale', 'rent' => 'Rent'])
-                    ->default('sale')
-                    ->required(),
-                Select::make('status')
-                    ->options([
-                    'available' => 'Available',
-                    'sold' => 'Sold',
-                    'pending' => 'Pending',
-                    'draft' => 'Draft',
-                    'rented' => 'Rented',
-                ])
-                    ->default('available')
-                    ->required(),
-                TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('#'),
-                TextInput::make('price_per_sqft')
-                    ->numeric(),
-                Textarea::make('address')
-                    ->required(),
-                TextInput::make('city')
-                    ->required(),
-                TextInput::make('state')
-                    ->required(),
-                TextInput::make('country')
-                    ->required()
-                    ->default('Nigeria'),
-                TextInput::make('postal_code'),
-                TextInput::make('latitude')
-                    ->numeric(),
-                TextInput::make('longitude')
-                    ->numeric(),
-                TextInput::make('bedrooms')
-                    ->numeric(),
-                TextInput::make('bathrooms')
-                    ->numeric(),
-                TextInput::make('total_area')
-                    ->numeric()
-                    ->prefix('sqft'),
 
-                TextInput::make('built_year')
-                    ->numeric(),
-                Toggle::make('furnished')
-                ->required(),
+                // 1. Section For Basic Informations
+                Section::make('Basic Information')
+                ->columnSpanFull()
+                    ->description('Provide the basic details of the property.')
+                    ->columns(2)
 
-                Toggle::make('parking')
-                ->live()
-                ->required(),
+                    ->schema(components: [
+                        Grid::make(2)
+                        ->columnSpanFull()
 
-                TextInput::make('parking_spaces')
-                    ->numeric()
-                    ->visible(condition:fn(Get $get): bool => $get('parking')),
+                            ->schema(components: [
+                                TextInput::make('title')
+                                    ->required(),
+                                TextInput::make('slug')
+                                    ->readOnly(),
+                            ]),
+                            Textarea::make('description')
+                            ->required()
+                            ->columnSpanFull(),
+
+                            // Another Grid
+                            Grid::make(3)
+                            ->columnSpanFull()
+                            ->schema([
+                                Select::make('type')
+                                ->options(Property::getPropertyTypes())
+                                ->required(),
+
+                                Select::make('listing_type')
+                                ->options(Property::getListingTypes())
+                                ->default('sale')
+                                ->required(),
+            
+                            Select::make('status')
+                                ->options(Property::getStatuses())
+                                ->default('available')
+                                ->required(),
+            
+                            ])
+                        ]),
+                    
+
+                    // 2. Section for Pricing
+                    Section::make('Pricing')
+
+                    ->columnSpanFull()
+                    ->description('Set the pricing details for the property.')
+                    ->schema([
+                        Grid::make(2)
+                        ->columnSpanFull()
+
+                        ->schema([
+                            TextInput::make('price')
+                                ->required()
+                                ->numeric()
+                                ->prefix('#'),
+                            TextInput::make('price_per_sqft')
+                                ->numeric()
+                                ->prefix('NGN')
+                                ->default(null),
+                        ]),
+                        
+                    ]),
+
+
+                    // 3. Section for the location
+
+                    Section::make('Location')
+                    ->columnSpanFull()
+                    ->description('Provide the location details of the property')
+                    ->schema([
+                        Grid::make(2)
+                        ->columnSpanFull()
+                        ->schema([
+                            Textarea::make('address')
+                            ->columnSpanFull()
+                            ->required(),
+                        TextInput::make('city')
+                            ->required(),
+                        TextInput::make('state')
+                            ->required(),
+                        TextInput::make('country')
+                            ->required()
+                            ->default('Nigeria'),
+                        TextInput::make('postal_code'),
+                        
+                        ]),
+                        
+                        Grid::make(2)
+                        ->columnSpanFull()
+                        ->schema([
+                            TextInput::make('latitude')
+                            ->numeric(),
+                        TextInput::make('longitude')
+                            ->numeric(),
+                        ]),
+                    ]),
+
+
+                    // 3. Section for property details
+                    Section::make('Details')
+                    ->columnSpanFull()
+                    ->description('Additional details about the property.')
+                    ->schema([
+                        Grid::make(4)
+                        ->columnSpanFull()
+                        ->schema([
+                            TextInput::make('bedrooms')
+                            ->numeric(),
+                        TextInput::make('bathrooms')
+                            ->numeric(),
+                        TextInput::make('total_area')
+                            ->numeric()
+                            ->prefix('sqft'),
+                        TextInput::make('built_year')
+                            ->numeric(),
+                        ]),
+
+                        Grid::make(2)
+                        ->columnSpanFull()
+                        ->schema([
+                            Toggle::make('furnished')
+                            ->required(),
+            
+                            Toggle::make('parking')
+                            ->live()
+                            ->required(),
+                        ]),
+
+                        Grid::make(2)
+                        ->columnSpanFull()
+                        ->schema([
+                            TagsInput::make('features')
+                            ->helperText('Add features like garden, pool, gym, etc'),
+                            TextInput::make('parking_spaces')
+                            ->numeric()
+                            ->visible(condition:fn(Get $get): bool => $get('parking')),
+                          
+                        ]),
+                        
+                    ]),
+
+                    
+                    //  4. Section for media and SEO details
+
+
+                    
                 
-                TagsInput::make('features')
-                ->helperText('Add features like garden, pool, gym, etc')
-                ->columnSpanFull(),
+       
+
+           
+
+                
+         
 
                 FileUpload::make('images')
                 ->multiple()
@@ -103,6 +188,7 @@ class PropertyForm
 
                 TextInput::make('slug')
                 ->readOnly(),
+
                 TextInput::make('meta_title'),
                 Textarea::make('meta_description')
                     ->columnSpanFull(),
