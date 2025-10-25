0595d5feceebe7b9e19d984bd32f6abf38c74683
Dabeey <abbasdaughter18@gmail.com>
2025-10-25 03:59:35 +0100

    Add enquiry model enhancements with fillable attributes and property relation

--- app/Models/Enquiry.php
+++ app/Models/Enquiry.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Enquiry extends Model
 {
     /** @use HasFactory<\Database\Factories\EnquiryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'property_id',
+        'name',
+        'email',
+        'phone',
+        'message',
+        'ip_address',
+        'user_agent',
+    ];
+
+    public function property(): BelongsTo{
+        return $this->belongsTo(Property::class);
+    }
 }
