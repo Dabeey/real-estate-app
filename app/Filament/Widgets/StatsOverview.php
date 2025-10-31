b9490c2b7e1c3aa38128079e91d15879f2a4df4e
Dabeey <abbasdaughter18@gmail.com>
2025-10-31 20:39:10 +0100

    Add statistics to StatsOverview widget for properties and enquiries count

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Filament\Widgets;
-
+use App\Models\Property;
+use App\Models\Enquiry;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +11,12 @@ class StatsOverview extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            //
+            stat::make('Number of Properties',Property::count()),
+            stat::make('Number of Enquiries',Enquiry::count()),
+            stat::make('Number of Available Properties',Property::where('status', 'available')->count()),
+            stat::make('Number of Rented Properties',Property::where('status', 'rented')->count()),
+            stat::make('Number of Sold Properties',Property::where('status', 'sold')->count()),
+
         ];
     }
 }
